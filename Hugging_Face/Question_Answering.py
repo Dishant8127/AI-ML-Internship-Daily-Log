2e5e0b8501cc6635af0fa62c7cfdb192521e0a85
Dishant@8127 <dishantpatel927@gmail.com>
2026-03-12 14:48:04 +0530

    as

--- Hugging_Face/Question_Answering.py
+++ Hugging_Face/Question_Answering.py
@@ -12,20 +12,3 @@ result = qa(question=question, context=context)
 
 print(result)
 
-
-
-# from transformers import pipeline
-
-# qa = pipeline(
-#     model="distilbert-base-cased-distilled-squad"
-# )
-
-# context = """
-# Hugging Face develops tools for building machine learning applications using transformers.
-# """
-
-# question = "What does Hugging Face develop?"
-
-# result = qa(question=question, context=context)
-
-# print(result["answer"])
