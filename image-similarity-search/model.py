6d365df6e67dcaff7b8a42e47e566720ff8b29cf
Dishant8127 <dishantpatel927@gmail.com>
2026-05-11 14:17:28 +0530

    image-similarity-search

--- image-similarity-search/model.py
+++ image-similarity-search/model.py
@@ -1,19 +1,9 @@
-# import open_clip
-
-# model, _, preprocess = open_clip.create_model_and_transforms(
-#     'ViT-B-32',
-#     pretrained='laion2b_s34b_b79k'
-# )
-
-
 from transformers import CLIPModel, CLIPProcessor
 
-# Load CLIP model
 model = CLIPModel.from_pretrained(
     "openai/clip-vit-base-patch32"
 )
 
-# Load CLIP processor
 processor = CLIPProcessor.from_pretrained(
     "openai/clip-vit-base-patch32"
 )
