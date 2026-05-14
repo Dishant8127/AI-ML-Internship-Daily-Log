49d0a7d762fccc018e5db19ecad8ec1ef421520f
Dishant8127 <dishantpatel927@gmail.com>
2026-05-14 15:58:28 +0530

    faiss_index_comparison

--- faiss_index_comparison/src/model.py
+++ faiss_index_comparison/src/model.py
@@ -10,9 +10,3 @@ model = CLIPModel.from_pretrained(
 processor = CLIPProcessor.from_pretrained(
     "openai/clip-vit-base-patch32"
 )
-
-
-
-# def normalize_embeddings(embeddings):
-#     norms = np.linalg.norm(embeddings, axis=1, keepdims=True)
-#     return embeddings / norms
